992afc32897834cd681076f8a92abe1705e6ba7a
Nermin Skenderovic <nermin@skenderovic.se>
2022-12-06 11:30:49 +0100

    d01

---
+++ src/d01/index.ts
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const data: number[][] = fs
+  .readFileSync('./data.txt', 'utf8')
+  .split('\n\n')
+  .map((elf: string) => elf.split('\n').map(Number))
+
+const sums = data.map((group) => group.reduce((acc, curr) => acc + curr, 0))
+
+const bestElf = sums.indexOf(Math.max(...sums))
+console.log(sums[bestElf])
+
+const top3 = sums.sort((a, b) => b - a).slice(0, 3)
+const top3sum = top3.reduce((acc, curr) => acc + curr, 0)
+console.log(top3sum)

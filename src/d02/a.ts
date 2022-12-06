cf34aa512a31be33379f14becf25b27a15736fe6
Nermin Skenderovic <nermin@skenderovic.se>
2022-12-06 12:37:08 +0100

    d02

---
+++ src/d02/a.ts
@@ -0,0 +1,78 @@
+import fs from 'fs'
+
+const data: string[][] = fs
+  .readFileSync(__dirname + '/data.txt', 'utf8')
+  .split('\n')
+  .map((row) => row.split(' '))
+
+const p1 = {
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+}
+
+const p2 = {
+  X: 'rock',
+  Y: 'paper',
+  Z: 'scissors',
+}
+
+const values = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+}
+
+let p2score = 0
+for (const row of data) {
+  const [p1move, p2move] = row
+  const p1shape = p1[p1move]
+  const p2shape = p2[p2move]
+  const p1value = values[p1shape]
+  const p2value = values[p2shape]
+
+  if (p1shape === 'rock') {
+    switch (p2shape) {
+      case 'rock': // draw
+        p2score += p2value + 3
+        break
+      case 'paper': // win
+        p2score += p2value + 6
+        break
+      case 'scissors': // lose
+        p2score += p2value + 0
+        break
+    }
+  }
+
+  if (p1shape === 'paper') {
+    switch (p2shape) {
+      case 'paper': // draw
+        p2score += p2value + 3
+        break
+      case 'scissors': // win
+        p2score += p2value + 6
+        break
+      case 'rock': // lose
+        p2score += p2value + 0
+        break
+    }
+  }
+
+  if (p1shape === 'scissors') {
+    switch (p2shape) {
+      case 'scissors': // draw
+        p2score += p2value + 3
+        break
+      case 'rock': // win
+        p2score += p2value + 6
+        break
+      case 'paper': // lose
+        p2score += p2value + 0
+        break
+    }
+  }
+}
+console.log(p2score)
+
+export {}
---
+++ src/d02/b.ts
@@ -0,0 +1,86 @@
+import fs from 'fs'
+
+const data: string[][] = fs
+  .readFileSync(__dirname + '/data.txt', 'utf8')
+  .split('\n')
+  .map((row) => row.split(' '))
+
+const p1 = {
+  A: 'rock',
+  B: 'paper',
+  C: 'scissors',
+}
+
+const p2 = {
+  X: 'lose',
+  Y: 'draw',
+  Z: 'win',
+}
+
+const values = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+}
+
+const results = {
+  lose: 0,
+  draw: 3,
+  win: 6,
+}
+
+let p2score = 0
+for (const row of data) {
+  const [p1move, p2move] = row
+  const p1shape = p1[p1move]
+  const p2result = p2[p2move]
+
+  let p2shape
+  let result
+  if (p1shape === 'rock') {
+    switch (p2result) {
+      case 'lose': // lose
+        p2shape = 'scissors'
+        result = 'lose'
+        break
+      case 'draw': // draw
+        p2shape = 'rock'
+        break
+      case 'win': // win
+        p2shape = 'paper'
+        break
+    }
+  }
+
+  if (p1shape === 'paper') {
+    switch (p2result) {
+      case 'lose': // lose
+        p2shape = 'rock'
+        break
+      case 'draw': // draw
+        p2shape = 'paper'
+        break
+      case 'win': // win
+        p2shape = 'scissors'
+        break
+    }
+  }
+
+  if (p1shape === 'scissors') {
+    switch (p2result) {
+      case 'lose': // lose
+        p2shape = 'paper'
+        break
+      case 'draw': // draw
+        p2shape = 'scissors'
+        break
+      case 'win': // win
+        p2shape = 'rock'
+        break
+    }
+  }
+  p2score += values[p2shape] + results[p2result]
+}
+console.log(p2score)
+
+export {}

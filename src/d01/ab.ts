67469e028111ac6d2e456182f68e380af8dca6d9
Nermin Skenderovic <nermin@skenderovic.se>
2022-12-06 12:04:02 +0100

    update d01

--- src/d01/ab.ts
+++ src/d01/ab.ts
@@ -1,7 +1,7 @@
-const fs = require('fs')
+import fs from 'fs'
 
 const data: number[][] = fs
-  .readFileSync('./data.txt', 'utf8')
+  .readFileSync(__dirname + '/data.txt', 'utf8')
   .split('\n\n')
   .map((elf: string) => elf.split('\n').map(Number))
 
@@ -13,3 +13,5 @@ console.log(sums[bestElf])
 const top3 = sums.sort((a, b) => b - a).slice(0, 3)
 const top3sum = top3.reduce((acc, curr) => acc + curr, 0)
 console.log(top3sum)
+
+export {}

e9093f3e2f29c12fb92bbe701414dd95a12cbea9
Nermin Skenderovic <nermin@skenderovic.se>
2022-12-08 11:08:08 +0100

    d03

---
+++ src/d03/ab.ts
@@ -0,0 +1,48 @@
+const fs = require('fs')
+
+const data = fs.readFileSync(__dirname + '/data.txt', 'utf8').split('\n')
+
+// A ∩ B ∩ ...
+const intersect = (setA, setB, ...args) => {
+  const result = new Set([...setA].filter((i) => setB.has(i)))
+  if (args.length === 0) return result
+  return intersect(result, args.shift(), ...args)
+}
+
+const getPriority = (letter) => {
+  if (letter === letter.toUpperCase()) {
+    return letter.charCodeAt(0) - ('A'.charCodeAt(0) - 1) + 26
+  } else {
+    return letter.charCodeAt(0) - ('a'.charCodeAt(0) - 1)
+  }
+}
+
+function a() {
+  let sum = 0
+  for (const row of data) {
+    const midIdx = Math.floor(row.length / 2)
+    const left = row.slice(0, midIdx)
+    const right = row.slice(midIdx)
+
+    const leftSet = new Set(left)
+    const rightSet = new Set(right)
+
+    const common = intersect(leftSet, rightSet)
+    const priority = getPriority([...common][0])
+    sum += priority
+  }
+  console.log(sum)
+}
+
+function b() {
+  let sum = 0
+  for (let i = 0; i < data.length; i += 3) {
+    const common = intersect(new Set(data[i]), new Set(data[i + 1]), new Set(data[i + 2]))
+    const priority = getPriority([...common][0])
+    sum += priority
+  }
+  console.log(sum)
+}
+
+a()
+b()

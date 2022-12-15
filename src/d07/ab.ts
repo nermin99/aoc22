9f0897ac825df445b68792e5df089a7dffb243b1
Nermin Skenderovic <nermin@skenderovic.se>
2022-12-15 17:01:41 +0100

    d07

---
+++ src/d07/ab.ts
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const _ = require('lodash')
+const data: string[] = fs.readFileSync(__dirname + '/data.txt', 'utf8').split('\n')
+
+data.shift()
+
+const tree: any = {}
+let cursor = '/'
+
+for (const io of data) {
+  // input
+  if (io.startsWith('$')) {
+    const [$, cmd, path] = io.split(' ')
+    if (cmd === 'cd') {
+      if (path === '..') {
+        cursor = cursor.slice(0, cursor.lastIndexOf('.'))
+      } else {
+        cursor += '.' + path
+      }
+    }
+  }
+  // output
+  else {
+    const [fileSize, fileName] = io.split(' ')
+    _.set(tree, `${cursor}.${fileName.replace('.', '_')}`, Number(fileSize))
+  }
+}
+
+const computeSizes = (obj) => {
+  let size = 0
+  for (const value of Object.values(obj)) {
+    if (typeof value === 'number') size += value
+    if (typeof value === 'object') size += computeSizes(value)
+  }
+  obj.SIZE = size
+  return size
+}
+computeSizes(tree)
+// console.dir(tree, { depth: null })
+
+const countTotalSizes = (obj) => {
+  let total = 0
+  for (const [key, value] of Object.entries(obj)) {
+    if (key === 'SIZE' && value <= 100000) total += value as number
+    if (typeof value === 'object') total += countTotalSizes(value)
+  }
+  return total
+}
+const total = countTotalSizes(tree)
+console.log('Part A: ' + total)
+
+// Part B
+const TOTAL_SPACE = 70000000
+const MIN_UNUSED_SPACE = 30000000
+const CUR_UNUSED_SPACE = TOTAL_SPACE - tree.SIZE
+const DIF_UNUSED_SPACE = MIN_UNUSED_SPACE - CUR_UNUSED_SPACE
+
+const dirSizes = []
+const findMinDirCandidates = (obj) => {
+  for (const [key, value] of Object.entries(obj)) {
+    if (key === 'SIZE' && value >= DIF_UNUSED_SPACE) dirSizes.push(value)
+    if (typeof value === 'object') findMinDirCandidates(value)
+  }
+}
+findMinDirCandidates(tree)
+
+const minDirSize = Math.min(...dirSizes)
+console.log('Part B: ' + minDirSize)
